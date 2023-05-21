6e3a3b6d4360aff40646747b223ec47e3fdbd469
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-21 11:00:14 -0300

    Frontend Profile

--- frontend/web/src/app/page.tsx
+++ frontend/web/src/app/page.tsx
@@ -1,8 +1,11 @@
 import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
 import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
 import { Signup } from '@/components/Signup'
+import { cookies } from 'next/headers'
 export default function Home() {
+  const isAuthenticated = cookies().has('token')
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
@@ -12,8 +15,7 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes"></div>
 
-        {/* Sign up */}
-        <Signup></Signup>
+        {isAuthenticated ? <Profile></Profile> : <Signup></Signup>}
 
         {/* Hero */}
         <Hero></Hero>
