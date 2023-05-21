42ff7b152dd89abba9791a640619cfd18b961856
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-21 13:07:49 -0300

    File upload and public static file serve

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,18 +1,31 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
+import fastifyMultipart from '@fastify/multipart'
 import 'dotenv/config'
-import { authRoutes } from './routes/auth'
+import { resolve } from 'node:path'
+
 const app = fastify()
 app.register(fastifyCors,{
   origin:true, // Unsafe - Remember to change that
 })
-app.register(memoriesRoutes)
-app.register(authRoutes)
+
+app.register(require('@fastify/static'),{
+  root: resolve(__dirname,'../uploads'),
+  prefix: '/uploads'
+})
+
+app.register(fastifyMultipart)
 app.register(fastifyJwt,{
   secret: 'spacetime' // Unsafe - Remember to change that
 })
+
+app.register(memoriesRoutes)
+app.register(authRoutes)
+app.register(uploadRoutes)
 app
   .listen({
     port: 3333,
