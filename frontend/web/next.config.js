cb479020bdaa0053bca58995632a06ca0cef4de7
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-21 14:26:41 -0300

    Frontend Web List Memories

--- frontend/web/next.config.js
+++ frontend/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'localhost', '127.0.0.1'],
   },
 }
 
