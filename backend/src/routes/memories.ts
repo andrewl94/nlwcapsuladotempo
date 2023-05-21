004d67e4d9b47a69503121e68f3dba0ff75567cc
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-21 14:27:16 -0300

    Memories return createdAt

--- backend/src/routes/memories.ts
+++ backend/src/routes/memories.ts
@@ -19,7 +19,8 @@ export async function memoriesRoutes(app: FastifyInstance) {
       return {
         id: memory.id,
         coverUrl: memory.coverUrl,
-        excerpt: memory.content.substring(0, 150).concat('...')
+        excerpt: memory.content.substring(0, 150).concat('...'),
+        createdAt: memory.createdAt
       }
     })
   })
