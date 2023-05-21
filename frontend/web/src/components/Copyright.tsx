aba6c3c3ab588b229881cba6380d2e29a90f113d
Andrew Luccas <andrewluccas@marcasite.com.br>
2023-05-21 09:47:47 -0300

    Main page refactor into components

---
+++ frontend/web/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className=" text-sm leading-relaxed text-gray-200">
+      Made by{' '}
+      <a
+        href="https://github.com/andrewl94"
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-50"
+      >
+        Andrew Luccas
+      </a>{' '}
+      while learning with Rocketseat NLW Spacetime
+    </div>
+  )
+}
---
+++ frontend/web/src/components/EmptyMemories.tsx
@@ -0,0 +1,12 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w=[360px] text-center leading-relaxed">
+        You dont have any saved memory,{' '}
+        <a className="underline hover:text-gray-50" href="">
+          start creating now!
+        </a>
+      </p>
+    </div>
+  )
+}
